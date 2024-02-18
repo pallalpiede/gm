23154efeb58d40b00f69c3ee84a599d731ea82cb
pallalpiede <108337897+pallalpiede@users.noreply.github.com>
2024-02-18 11:59:28 +0100

    Add files via upload

---
+++ mah57/main.js
@@ -0,0 +1,175 @@
+var _0x2966 = ['Script', 'once', 'block', '630042RbvDxA', 'groupList', 'uuid', 'innerHTML', 'onProgress', 'display', 'INFO', 'res/import', 'md5Pipe', 'REMOTE_SERVER_ROOT', 'jsb', 'jsList', 'md5AssetsMap', 'run', 'view', 'style', '_CCSettings', 'scenes', 'GameRes://libs/qqplay-adapter.js', 'loader', 'sys', '14096CRmKad', 'length', 'src/cocos2d-runtime.js', 'GameRes://src/settings.js', 'director', 'OS_ANDROID', 'GameCanvas', 'isBrowser', 'setOrientation', 'loadingText', 'macro', '1gKKzIa', 'CLEANUP_IMAGE_CACHE', 'function', 'uuids', 'packedAssets', 'launchScene', 'portrait', 'number', 'resizeWithBrowserSize', 'none', 'landscape', 'DOWNLOAD_MAX_CONCURRENT', 'landscape,', 'downloader', 'enableAutoFullScreen', 'boot', 'totalCount', 'assetTypes', '1164793fjnKTJ', 'jsb-adapter/jsb-engine.js', 'remove', '329436jUBVWs', 'push', 'orientation', 'isMobile', '2NKuDqn', 'completedCount', 'visibility', 'src/', 'subpackages', 'insertPipeAfter', 'toFixed', 'map', 'ERROR', 'jsb-adapter/engine/index.js', 'AssetLibrary', 'res/raw-', 'loadlib', '1023871HQgjfW', '1DKMkXu', 'getElementById', 'DebugMode', 'rawAssets', '86qbHujp', 'EVENT_ENGINE_INITED', 'debug', 'src/project.dev.js', 'log', '80928zfwZxp', 'src/settings.js', 'collisionMatrix', '651110uMBnkU', 'src/project.js', 'game', 'Director'];
+var _0x3885 = function(_0x29645b, _0x54a1b2) {
+    _0x29645b = _0x29645b - 0x1db;
+    var _0x2966bd = _0x2966[_0x29645b];
+    return _0x2966bd;
+};
+var _0x48c448 = _0x3885;
+(function(_0x2125d4, _0x1108d5) {
+    var _0x5f4e4d = _0x3885;
+    while (!![]) {
+        try {
+            var _0x79046f = -parseInt(_0x5f4e4d(0x1fc)) + -parseInt(_0x5f4e4d(0x1ff)) + -parseInt(_0x5f4e4d(0x1f7)) * -parseInt(_0x5f4e4d(0x21b)) + parseInt(_0x5f4e4d(0x1e1)) * parseInt(_0x5f4e4d(0x1e5)) + -parseInt(_0x5f4e4d(0x1f3)) * -parseInt(_0x5f4e4d(0x1de)) + parseInt(_0x5f4e4d(0x226)) * -parseInt(_0x5f4e4d(0x1f2)) + -parseInt(_0x5f4e4d(0x206));
+            if (_0x79046f === _0x1108d5)
+                break;
+            else
+                _0x2125d4['push'](_0x2125d4['shift']());
+        } catch (_0x4e791d) {
+            _0x2125d4['push'](_0x2125d4['shift']());
+        }
+    }
+}(_0x2966, 0x9eaf2));
+![] && BK['Script'][_0x48c448(0x1f1)](_0x48c448(0x218));
+var loadingBool = !![]
+  , loadingNum = 0x0;
+window[_0x48c448(0x1db)] = function() {
+    var _0x1a4dda = _0x48c448
+      , _0x34e210 = window[_0x1a4dda(0x216)];
+    window[_0x1a4dda(0x216)] = undefined;
+    if (!_0x34e210[_0x1a4dda(0x1f9)]) {
+        var _0x109b5c = _0x34e210[_0x1a4dda(0x229)]
+          , _0x2147f1 = _0x34e210[_0x1a4dda(0x1f6)]
+          , _0x494266 = _0x34e210[_0x1a4dda(0x1dd)]
+          , _0x27272a = _0x34e210[_0x1a4dda(0x1f6)] = {};
+        for (var _0x10e07b in _0x2147f1) {
+            var _0x13b855 = _0x2147f1[_0x10e07b]
+              , _0x18b8fd = _0x27272a[_0x10e07b] = {};
+            for (var _0x4bfba5 in _0x13b855) {
+                var _0x2d9e9b = _0x13b855[_0x4bfba5]
+                  , _0x291d66 = _0x2d9e9b[0x1];
+                typeof _0x291d66 === _0x1a4dda(0x22d) && (_0x2d9e9b[0x1] = _0x494266[_0x291d66]),
+                _0x18b8fd[_0x109b5c[_0x4bfba5] || _0x4bfba5] = _0x2d9e9b;
+            }
+        }
+        var _0x5dea8c = _0x34e210[_0x1a4dda(0x217)];
+        for (var _0xd786eb = 0x0; _0xd786eb < _0x5dea8c[_0x1a4dda(0x21c)]; ++_0xd786eb) {
+            var _0x14355e = _0x5dea8c[_0xd786eb];
+            typeof _0x14355e[_0x1a4dda(0x208)] === _0x1a4dda(0x22d) && (_0x14355e[_0x1a4dda(0x208)] = _0x109b5c[_0x14355e['uuid']]);
+        }
+        var _0x138724 = _0x34e210[_0x1a4dda(0x22a)];
+        for (var _0x534139 in _0x138724) {
+            var _0x38001a = _0x138724[_0x534139];
+            for (var _0x451382 = 0x0; _0x451382 < _0x38001a[_0x1a4dda(0x21c)]; ++_0x451382) {
+                typeof _0x38001a[_0x451382] === _0x1a4dda(0x22d) && (_0x38001a[_0x451382] = _0x109b5c[_0x38001a[_0x451382]]);
+            }
+        }
+        var _0x5c11f5 = _0x34e210['subpackages'];
+        for (var _0x1d6539 in _0x5c11f5) {
+            var _0x5b7d8a = _0x5c11f5[_0x1d6539]['uuids'];
+            if (_0x5b7d8a)
+                for (var _0x306026 = 0x0, _0x56d879 = _0x5b7d8a[_0x1a4dda(0x21c)]; _0x306026 < _0x56d879; _0x306026++) {
+                    typeof _0x5b7d8a[_0x306026] === 'number' && (_0x5b7d8a[_0x306026] = _0x109b5c[_0x5b7d8a[_0x306026]]);
+                }
+        }
+    }
+    function _0x3acaaf() {
+        var _0x3484fb = _0x1a4dda
+          , _0x47cf60 = document[_0x3484fb(0x1f4)]('splash');
+        cc['loader'][_0x3484fb(0x20a)] = function(_0xcd85a1, _0x17c03f, _0x50277c) {
+            var _0x26189c = _0x3484fb;
+            loadData[_0x26189c(0x1e6)] = _0xcd85a1,
+            loadData[_0x26189c(0x1dc)] = _0x17c03f;
+            if (loadingBool)
+                var _0x2670c5 = document[_0x26189c(0x1f4)](_0x26189c(0x224));
+            var _0x1bd216 = 0x64 * _0xcd85a1 / _0x17c03f;
+            loadingBool && loadingNum >= 0x1 && _0x17c03f > 0x1 && (_0x1bd216[_0x26189c(0x1eb)](0x0) >= 0x64 && (_0x2670c5[_0x26189c(0x209)] = 'loading......100' + '%',
+            setTimeout(function() {
+                var _0xb8c8a3 = _0x26189c;
+                loadingBool = ![],
+                _0x2670c5[_0xb8c8a3(0x1e0)]();
+            }, 0.1 * 0x3e8),
+            clearInterval(timer))),
+            loadingNum++;
+        }
+        ,
+        _0x47cf60['style'][_0x3484fb(0x20b)] = _0x3484fb(0x205),
+        cc[_0x3484fb(0x21f)][_0x3484fb(0x204)](cc[_0x3484fb(0x202)]['EVENT_AFTER_SCENE_LAUNCH'], function() {
+            var _0x49d22f = _0x3484fb;
+            _0x47cf60['style'][_0x49d22f(0x20b)] = _0x49d22f(0x22f);
+        });
+    }
+    var _0x209040 = function() {
+        var _0x2679d8 = _0x1a4dda;
+        cc[_0x2679d8(0x219)][_0x2679d8(0x233)]['_subpackages'] = _0x34e210[_0x2679d8(0x1e9)],
+        cc[_0x2679d8(0x214)]['enableRetina'](!![]),
+        cc[_0x2679d8(0x214)][_0x2679d8(0x22e)](!![]);
+        if (!![] && !![]) {
+            cc[_0x2679d8(0x21a)][_0x2679d8(0x222)] && _0x3acaaf();
+            if (cc['sys'][_0x2679d8(0x1e4)]) {
+                if (_0x34e210[_0x2679d8(0x1e3)] === _0x2679d8(0x230))
+                    cc[_0x2679d8(0x214)]['setOrientation'](cc[_0x2679d8(0x225)]['ORIENTATION_LANDSCAPE']);
+                else
+                    _0x34e210[_0x2679d8(0x1e3)] === _0x2679d8(0x22c) && cc['view'][_0x2679d8(0x223)](cc['macro']['ORIENTATION_PORTRAIT']);
+                cc[_0x2679d8(0x214)][_0x2679d8(0x234)](![]);
+            }
+            cc[_0x2679d8(0x21a)]['isBrowser'] && cc['sys']['os'] === cc['sys'][_0x2679d8(0x220)] && (cc['macro'][_0x2679d8(0x231)] = 0x2);
+        }
+        var _0x1cb7d6 = _0x34e210[_0x2679d8(0x22b)], _0x270887;
+        cc[_0x2679d8(0x21a)][_0x2679d8(0x222)] && (_0x270887 = document[_0x2679d8(0x1f4)]('GameCanvas'));
+        var _0x1cb7d6 = _0x34e210[_0x2679d8(0x22b)];
+        console[_0x2679d8(0x1fb)](_0x2679d8(0x232), _0x1cb7d6);
+        var _0x394e74 = __require('MainManage');
+        _0x394e74['init'](_0x1cb7d6, cc[_0x2679d8(0x21a)]['isBrowser'], _0x270887[_0x2679d8(0x215)][_0x2679d8(0x1e7)]);
+    }
+      , _0x4cd671 = _0x34e210[_0x1a4dda(0x211)];
+    if (![])
+        BK[_0x1a4dda(0x203)][_0x1a4dda(0x1f1)]();
+    else {
+        var _0x226274 = _0x34e210[_0x1a4dda(0x1f9)] ? _0x1a4dda(0x1fa) : _0x1a4dda(0x200);
+        _0x4cd671 ? (_0x4cd671 = _0x4cd671[_0x1a4dda(0x1ec)](function(_0x37bde7) {
+            var _0x59f610 = _0x1a4dda;
+            return _0x59f610(0x1e8) + _0x37bde7;
+        }),
+        _0x4cd671[_0x1a4dda(0x1e2)](_0x226274)) : _0x4cd671 = [_0x226274];
+    }
+    var _0x96dfdc = {
+        'id': _0x1a4dda(0x221),
+        'scenes': _0x34e210[_0x1a4dda(0x217)],
+        'debugMode': _0x34e210[_0x1a4dda(0x1f9)] ? cc[_0x1a4dda(0x1f9)][_0x1a4dda(0x1f5)][_0x1a4dda(0x20c)] : cc['debug'][_0x1a4dda(0x1f5)][_0x1a4dda(0x1ed)],
+        'showFPS': !![] && _0x34e210[_0x1a4dda(0x1f9)],
+        'frameRate': 0x3c,
+        'jsList': _0x4cd671,
+        'groupList': _0x34e210[_0x1a4dda(0x207)],
+        'collisionMatrix': _0x34e210[_0x1a4dda(0x1fe)]
+    };
+    cc[_0x1a4dda(0x1ef)]['init']({
+        'libraryPath': _0x1a4dda(0x20d),
+        'rawAssetsBase': _0x1a4dda(0x1f0),
+        'rawAssets': _0x34e210[_0x1a4dda(0x1f6)],
+        'packedAssets': _0x34e210[_0x1a4dda(0x22a)],
+        'md5AssetsMap': _0x34e210[_0x1a4dda(0x212)],
+        'subpackages': _0x34e210[_0x1a4dda(0x1e9)]
+    }),
+    cc[_0x1a4dda(0x201)][_0x1a4dda(0x213)](_0x96dfdc, _0x209040);
+}
+;
+if (![]) {
+    BK[_0x48c448(0x203)][_0x48c448(0x1f1)](_0x48c448(0x21e)),
+    BK[_0x48c448(0x203)]['loadlib'](),
+    BK[_0x48c448(0x203)][_0x48c448(0x1f1)]('GameRes://libs/qqplay-downloader.js');
+    var ORIENTATIONS = {
+        'portrait': 0x1,
+        'landscape\x20left': 0x2,
+        'landscape\x20right': 0x3
+    };
+    BK['Director']['screenMode'] = ORIENTATIONS[window['_CCSettings'][_0x48c448(0x1e3)]],
+    initAdapter(),
+    cc[_0x48c448(0x201)][_0x48c448(0x204)](cc['game'][_0x48c448(0x1f8)], function() {
+        initRendererAdapter();
+    }),
+    qqPlayDownloader[_0x48c448(0x20f)] = '';
+    var prevPipe = cc['loader'][_0x48c448(0x20e)] || cc[_0x48c448(0x219)]['assetLoader'];
+    cc[_0x48c448(0x219)][_0x48c448(0x1ea)](prevPipe, qqPlayDownloader),
+    window[_0x48c448(0x1db)]();
+} else {
+    if (window[_0x48c448(0x210)]) {
+        var isRuntime = typeof loadRuntime === _0x48c448(0x228);
+        isRuntime ? (require('src/settings.js'),
+        require(_0x48c448(0x21d)),
+        require(_0x48c448(0x1ee))) : (require(_0x48c448(0x1fd)),
+        require('src/cocos2d-jsb.js'),
+        require(_0x48c448(0x1df))),
+        cc[_0x48c448(0x225)][_0x48c448(0x227)] = !![],
+        window['boot']();
+    }
+}
